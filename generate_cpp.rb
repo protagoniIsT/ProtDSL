1fd670622450a16cf6e12de1155b1d4b551c5052
vboxuser <gordienkokosta1@gmail.com>
2026-01-22 13:03:01 +0300

    added exit code output

--- generate_cpp.rb
+++ generate_cpp.rb
@@ -570,13 +570,16 @@ class CppGenerator
         code = <<~CPP
         // Auto generated from ProtDSL
         #include <cstdio>
+        #include <iostream>
         #include "machine.h"
 
         int main(int argc, char* argv[]) {
             if (argc < 2) { fprintf(stderr, "Usage: %s <elf>\\n", argv[0]); return 1; }
             Machine m;
             if (!m.load_elf(argv[1])) { fprintf(stderr, "Failed to load: %s\\n", argv[1]); return 1; }
-            return m.run();
+            int exit_code = m.run();
+            std::cerr << "Exit code " << exit_code << std::endl;
+            return exit_code;
         }
         CPP
 
